905a1a88d5cf2792405542211b403166ff377103
Megan Swenson <mrsmeganswenson@gmail.com>
2020-08-28 15:07:02 -0600

    First commit

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is the start of my portfolio
+      </header>
+    </div>
+  );
+}
+
+export default App;
