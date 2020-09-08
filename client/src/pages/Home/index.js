4cfda39b6fa61ef117aeb4ecb839ee0ce0d147b5
Megan Swenson <mrsmeganswenson@gmail.com>
2020-09-08 13:39:00 -0600

    Starting on home page

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./style.css";
+import Navigation from "../../components/Navbar";
+
+class Home extends Component {
+    state = {
+        
+    };
+
+    // Functions for home page
+
+    render() {
+        return (
+            <>
+                <Navigation />
+            </>
+        );
+    }
+}
+
+export default Home;
